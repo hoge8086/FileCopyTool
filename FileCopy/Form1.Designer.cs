a2cd47cfb39d6f2ddbb558350a17e9792dcf8824
hoge8086 <hoge8086@gmail.com>
2017-07-21 00:41:44 +0900

    UI画面編集

--- FileCopy/Form1.Designer.cs
+++ FileCopy/Form1.Designer.cs
@@ -36,83 +36,116 @@
             this.button_Paste = new System.Windows.Forms.Button();
             this.button_Delete = new System.Windows.Forms.Button();
             this.button_Copy = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(21, 12);
+            this.textBox1.Location = new System.Drawing.Point(103, 12);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(360, 19);
+            this.textBox1.Size = new System.Drawing.Size(298, 19);
             this.textBox1.TabIndex = 0;
             // 
             // button_SeleceFolder
             // 
-            this.button_SeleceFolder.Location = new System.Drawing.Point(387, 10);
+            this.button_SeleceFolder.Location = new System.Drawing.Point(407, 10);
             this.button_SeleceFolder.Name = "button_SeleceFolder";
-            this.button_SeleceFolder.Size = new System.Drawing.Size(65, 23);
+            this.button_SeleceFolder.Size = new System.Drawing.Size(45, 23);
             this.button_SeleceFolder.TabIndex = 1;
-            this.button_SeleceFolder.Text = "button1";
+            this.button_SeleceFolder.Text = "参照";
             this.button_SeleceFolder.UseVisualStyleBackColor = true;
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 71);
+            this.dataGridView1.Location = new System.Drawing.Point(14, 71);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 21;
-            this.dataGridView1.Size = new System.Drawing.Size(431, 246);
+            this.dataGridView1.Size = new System.Drawing.Size(438, 246);
             this.dataGridView1.TabIndex = 2;
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(12, 323);
+            this.checkBox1.Location = new System.Drawing.Point(14, 323);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(80, 16);
+            this.checkBox1.Size = new System.Drawing.Size(370, 16);
             this.checkBox1.TabIndex = 3;
-            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.Text = "起点フォルダ配下のコピー元は下位のディレクトリ構造を保持してコピーする";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(12, 345);
+            this.textBox2.Location = new System.Drawing.Point(88, 345);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(431, 19);
+            this.textBox2.Size = new System.Drawing.Size(364, 19);
             this.textBox2.TabIndex = 4;
             // 
             // button_Paste
             // 
-            this.button_Paste.Location = new System.Drawing.Point(21, 42);
+            this.button_Paste.Location = new System.Drawing.Point(316, 42);
             this.button_Paste.Name = "button_Paste";
-            this.button_Paste.Size = new System.Drawing.Size(75, 23);
+            this.button_Paste.Size = new System.Drawing.Size(65, 23);
             this.button_Paste.TabIndex = 5;
-            this.button_Paste.Text = "button2";
+            this.button_Paste.Text = "貼り付け";
             this.button_Paste.UseVisualStyleBackColor = true;
             // 
             // button_Delete
             // 
-            this.button_Delete.Location = new System.Drawing.Point(102, 42);
+            this.button_Delete.Location = new System.Drawing.Point(387, 42);
             this.button_Delete.Name = "button_Delete";
-            this.button_Delete.Size = new System.Drawing.Size(75, 23);
+            this.button_Delete.Size = new System.Drawing.Size(65, 23);
             this.button_Delete.TabIndex = 6;
-            this.button_Delete.Text = "button3";
+            this.button_Delete.Text = "削除";
             this.button_Delete.UseVisualStyleBackColor = true;
             // 
             // button_Copy
             // 
-            this.button_Copy.Location = new System.Drawing.Point(377, 370);
+            this.button_Copy.Location = new System.Drawing.Point(350, 370);
             this.button_Copy.Name = "button_Copy";
-            this.button_Copy.Size = new System.Drawing.Size(75, 23);
+            this.button_Copy.Size = new System.Drawing.Size(102, 30);
             this.button_Copy.TabIndex = 7;
-            this.button_Copy.Text = "button4";
+            this.button_Copy.Text = "コピー実行";
             this.button_Copy.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 12);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "コピー先フォルダ：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 12);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "コピー元ファイル/フォルダ：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 348);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 12);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "起点フォルダ：";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(464, 405);
+            this.ClientSize = new System.Drawing.Size(464, 412);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button_Copy);
             this.Controls.Add(this.button_Delete);
             this.Controls.Add(this.button_Paste);
@@ -122,7 +155,7 @@
             this.Controls.Add(this.button_SeleceFolder);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "FileCopyTool";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -139,6 +172,9 @@
         private System.Windows.Forms.Button button_Paste;
         private System.Windows.Forms.Button button_Delete;
         private System.Windows.Forms.Button button_Copy;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
